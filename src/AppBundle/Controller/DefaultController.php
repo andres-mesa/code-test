fe3733c22cf17f41559548929d57ceed4e14e183
AMESA <andres.mesa@tudespensa.com>
2019-05-30 13:34:08 +0200

    Serializer y FOS Rest Bundle

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,9 +3,11 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Clientes;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Annotation\MaxDepth;
 
 class DefaultController extends Controller
 {
@@ -16,8 +18,26 @@ class DefaultController extends Controller
     {
 
 
-        $cliente = new Clientes();
-        $cliente->setNombre("Andrés");
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository(Clientes::class)->findAll();
+
+
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/pedido", name="homepage")
+     * @Method("POST")
+     */
+    public function postAction(Request $request)
+    {
+
+
+        $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository(Clientes::class)->findAll();
 
 
         // replace this example code with whatever you need
